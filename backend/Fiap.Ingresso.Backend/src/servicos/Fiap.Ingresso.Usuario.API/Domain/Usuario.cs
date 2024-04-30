19befe5e8b2462322226c98340f6fe65338c3858
rafael pontin <rfmpontin@gmail.com>
2024-04-29 22:43:52 -0300

    feat: inicio projeto

---
+++ backend/Fiap.Ingresso.Backend/src/servicos/Fiap.Ingresso.Usuario.API/Domain/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace Fiap.Ingresso.Usuario.API.Domain;
+
+public class Usuario
+{
+    public Guid Id { get; set; }
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+
+}
