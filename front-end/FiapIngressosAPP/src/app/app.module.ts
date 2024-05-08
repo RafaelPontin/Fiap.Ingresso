25d2dbe049710e61ae7cb2df41358abde305a64c
Alexssander <alexssanderferreira@hotmail.com>
2024-05-07 21:35:20 -0300

    iniciando front

---
+++ front-end/FiapIngressosAPP/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { UserComponent } from './components/user/user.component';
+import { NavComponent } from './shared/nav/nav.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UserComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
