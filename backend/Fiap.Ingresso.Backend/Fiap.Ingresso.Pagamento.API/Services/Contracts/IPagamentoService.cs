9cbe8df6efde6833fa4e2d3d697379f44fc2f1b4
rafael pontin <rfmpontin@gmail.com>
2024-05-08 12:01:11 -0300

    feat(pagamento-teste): teste pagamento

--- backend/Fiap.Ingresso.Backend/Fiap.Ingresso.Pagamento.API/Services/Contracts/IPagamentoService.cs
+++ backend/Fiap.Ingresso.Backend/Fiap.Ingresso.Pagamento.API/Services/Contracts/IPagamentoService.cs
@@ -5,7 +5,7 @@ using Fiap.Ingresso.WebAPI.Core.Communication;
 namespace Fiap.Ingresso.Pagamento.API.Services.Contracts;
 public interface IPagamentoService
 {
-    Task<ResponseResult<Guid>> CadastraEvento(CadastrarPagamento pagamentoDto);
+    Task<ResponseResult<Guid>> CadastraPagamento(CadastrarPagamento pagamentoDto);
     Task<ResponseResult<EPagamento>> GetPagamentoById(Guid id);
     Task<ResponseResult<string>> GetLinhaDigitavel(Guid id);
 }
