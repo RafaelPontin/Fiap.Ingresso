665dfc697da402a3821e4d5733d6cb6b4bfbc414
Elielson Rodrigues <elielson.rodrigues@fmu.br>
2024-05-13 21:47:22 -0300

    feat: retornar usuario logado

---
+++ backend/Fiap.Ingresso.Backend/src/servicos/Fiap.Ingresso.Usuario.API/DTOs/UsuarioLogadoResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Fiap.Ingresso.Usuario.API.DTOs
+{
+    public class UsuarioLogadoResponse
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }        
+        public string Email { get; set; }
+        public string AccessToken { get; set; }
+
+    }
+}
